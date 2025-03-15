578bd80dec5014c31a223114a374c4849c70d0ff
Bikerrrr <hieuttps37551@edu.fpt.vn>
2025-03-15 21:57:59 +0700

    bikerrr

--- src/Screens/DetailPlan.jsx
+++ src/Screens/DetailPlan.jsx
@@ -1,8 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef, useEffect, useState, useContext } from 'react';
+import { 
+  StyleSheet, 
+  View, 
+  Text, 
+  Animated, 
+  Image, 
+  TouchableOpacity, 
+  ScrollView, 
+  StatusBar,
+  Dimensions,
+  ActivityIndicator,
+  ToastAndroid 
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { ChitietPlan, resetChitietPlan } from '../redux/ChitietPlanSlice';
 
-const DetailPlan = () => {
+
+
+const DetailPlan = (props) => {
+
+  const { navigation, route } = props;
+  const { DetailPlanId } = route?.params || {};
+  const dispatch = useDispatch();
+  const { ChitietPlanData, ChitietPlanStatus, error } = useSelector(state => state.chitietplan);
+
+
+  
+
   return (
     <SafeAreaView>
       <Text>DetailPlan</Text>
